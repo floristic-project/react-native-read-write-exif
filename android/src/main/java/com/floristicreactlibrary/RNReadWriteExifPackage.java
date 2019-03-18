e16fd571b8cba8179ede35532b154aa99d97a27a
AntoineAA <antoine.affouard@gmail.com>
2019-03-18 15:11:29 +0100

    add sanselan as jar lib

--- android/src/main/java/com/floristicreactlibrary/RNReadWriteExifPackage.java
+++ android/src/main/java/com/floristicreactlibrary/RNReadWriteExifPackage.java
@@ -1,15 +1,15 @@
 
 package com.floristicreactlibrary;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /*
  * Register the Module; this happens in the createNativeModules of your apps package. If a module
